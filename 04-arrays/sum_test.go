f1f424772692cac593af41b1c4bbb30c4b6dafb5
Marco Marchini <775639+marcom04@users.noreply.github.com>
2025-03-23 21:43:26 +0100

    Add 04-arrays.

---
+++ 04-arrays/sum_test.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"slices"
+	"testing"
+)
+
+func TestSum(t *testing.T) {
+
+	t.Run("collection of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+
+		got := Sum(numbers)
+		want := 6
+
+		if got != want {
+			t.Errorf("got %d want %d, given %v", got, want, numbers)
+		}
+	})
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	assertSlicesEqual(t, got, want)
+}
+
+func TestSumAllTails(t *testing.T) {
+
+	t.Run("make the sums of some slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+
+		assertSlicesEqual(t, got, want)
+	})
+
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
+
+		assertSlicesEqual(t, got, want)
+	})
+
+}
+
+func assertSlicesEqual(t testing.TB, got, want []int) {
+	t.Helper()
+	if !slices.Equal(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
